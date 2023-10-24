bd19fe914aaa5aefd67d87308d525170c0942a3f
Grachunya <kingiv2312@gmail.com>
2023-10-24 18:43:54 +0300

    repeated commit

--- pz3/PZ_3.2.py
+++ pz3/PZ_3.2.py
@@ -1,20 +1,25 @@
 # Смоделировать пройстейший калькулятор,умеющий выполнять 4 основные арифметические операции
+while True:
+    try:
+        num1 = float(input("Введите первое число: "))
+        operator = input("Введите оператор (+, -, *, /): ")
+        num2 = float(input("Введите второе число: "))
+        if operator == "+":
+            result = num1 + num2
+        elif operator == "-":
+            result = num1 - num2
+        elif operator == "*":
+            result = num1 * num2
+        elif operator == "/":
+            if num2 == 0:
+                print("Ошибка: деление на ноль!")
+                exit()
+            result = num1 / num2
+        else:
+            print("Ошибка: неверный оператор!")
+            exit()
+        print("Результат:", result)
+        break
 
-num1 = float(input("Введите первое число: "))
-operator = input("Введите оператор (+, -, *, /): ")
-num2 = float(input("Введите второе число: "))
-if operator == "+":
-    result = num1 + num2
-elif operator == "-":
-    result = num1 - num2
-elif operator == "*":
-    result = num1 * num2
-elif operator == "/":
-    if num2 == 0:
-        print("Ошибка: деление на ноль!")
-        exit()
-    result = num1 / num2
-else:
-    print("Ошибка: неверный оператор!")
-    exit()
-print("Результат:", result)
+    except ValueError:  # обработка исключений
+        print("Ошибка! Введите число")
