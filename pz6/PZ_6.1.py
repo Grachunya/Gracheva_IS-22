ba7fe021f2fd178d6c340df4f3e8c43d662654c2
Grachunya <kingiv2312@gmail.com>
2023-12-12 17:50:24 +0300

    meo commit

--- pz6/PZ_6.1.py
+++ pz6/PZ_6.1.py
@@ -1,10 +1,10 @@
 
-# Даны целые числа N (>2), A и B
+# Даны целые числа A и B
 # Сформировать и вывести целочисленный список размера 10
 # первый элемент которого равен A, второй равен B
 # а каждый последующий элемент равен сумме всех предыдущих.
 
-def FirstList(A, B, N):
+def FirstList(A, B):
     result = [A, B]
     for i in range(2, 10):
         result.append(sum(result[:i]))
@@ -13,4 +13,4 @@ def FirstList(A, B, N):
 A = 3
 B = 4
 
-print(FirstList(A, B, 2))
+print(FirstList(A, B))
