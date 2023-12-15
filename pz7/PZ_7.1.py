3947b96bba6261150f32d8165eb333c1761daeb2
Grachunya <kingiv2312@gmail.com>
2023-12-15 19:23:58 +0300

    meow commit

--- pz7/PZ_7.1.py
+++ pz7/PZ_7.1.py
@@ -3,10 +3,10 @@
 # если латинская буква - выводится строка "lat"
 # если русская - выводится строка "rus"
 
-s = "C"
-if ord("C") == ord(s):
+s = "ф"
+if ord('A') <= ord(s) <= ord('Z') or ord('a') <= ord(s) <= ord('z'):
     print("lat")
-elif ord("C") == ord(s):
+elif ord('а') <= ord(s) <= ord('я') or ord('А') <= ord(s) <= ord('Я'):
     print("rus")
 else:
     print("digit")
